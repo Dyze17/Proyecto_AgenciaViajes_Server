eb0ae8501c262cff3884e1309b4fa07d96eed510
saskrak <117883713+saskrak@users.noreply.github.com>
2023-11-18 17:52:59 -0500

    logica de modificar perfil echa faltaria probarla, logica de agregar reserva echa, agregue algunos metodos como crear reserva y modificar usuario en la clase principal, utilicce los log y lei y escribi archivos, faltaria que el metodo modificarusuario sobre escriba el metodo y ya

--- src/main/java/Controladores/PrincipalController.java
+++ src/main/java/Controladores/PrincipalController.java
@@ -1,8 +1,11 @@
 package Controladores;
 
+import Utils.ArchivoUtils;
+import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import javafx.scene.Node;
+import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.scene.layout.AnchorPane;
 import java.net.URL;
@@ -29,9 +32,10 @@ public class PrincipalController implements Initializable {
         instance = this;
     }
 
+    @FXML
     public void showReserva() {
         try {
-            Node node = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("/Interfaces/Reserva.fxml")));
+            Node node = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("/Interfaces/Reserva2.fxml")));
             panelFormulario.getChildren().setAll(node);
         } catch (Exception e) {
             System.out.println(e.getMessage());
@@ -40,6 +44,7 @@ public class PrincipalController implements Initializable {
 
     public void showModificar (){
         try {
+            ArchivoUtils.mostrarMensaje("Alerta", "Modificacion de perfil", "Para poder modificar tu perfil, la identificacion debe ser la misma con la que te registraste", Alert.AlertType.INFORMATION);
             Node node = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("/Interfaces/Modificar.fxml")));
             panelFormulario.getChildren().setAll(node);
         } catch (Exception e) {
--- src/main/java/Controladores/ReservaAgregarController.java
+++ src/main/java/Controladores/ReservaAgregarController.java
@@ -1,19 +1,24 @@
 package Controladores;
 
-import Modelo.AgenciaUQ;
-import Modelo.GuiaTuristico;
+import Exceptions.AtributoVacioException;
+import Exceptions.CupoInvalidoException;
+import Exceptions.ErrorGuardarCambios;
+import Exceptions.FechaNoValidaException;
+import Modelo.*;
+import Utils.ArchivoUtils;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
-import javafx.scene.control.Button;
-import javafx.scene.control.ChoiceBox;
-import javafx.scene.control.DatePicker;
-import javafx.scene.control.TextField;
+import javafx.scene.control.*;
 
 import java.io.IOException;
 import java.net.URL;
+import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.ResourceBundle;
+import Enum.EstadoReserva;
+import javafx.scene.layout.AnchorPane;
+import Enum.Idioma;
 import Enum.EstadoReserva;
 
 public class ReservaAgregarController implements Initializable {
@@ -29,13 +34,18 @@ public class ReservaAgregarController implements Initializable {
     @FXML
     private ChoiceBox<String> boxGuia;
     @FXML
-    private TextField txtPaquete;
+    private ChoiceBox<String> boxPaquete;
+    @FXML
+    private AnchorPane paneReservaAgregar;
     @FXML
     private DatePicker dateSolicitud;
     @FXML
     private TextField txtDocumento;
     @FXML
     private DatePicker dateViaje;
+
+    private Cliente cliente;
+    private Reserva reserva;
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
@@ -54,13 +64,52 @@ public class ReservaAgregarController implements Initializable {
                 } catch (IOException e) {
                     throw new RuntimeException(e);
                 }
+                try {
+                    ArrayList<String> nombresPaquete = AgenciaUQ.leerNombresPaquetesTuristicos();
+                    for(String nombre : nombresPaquete) {
+                        boxPaquete.getItems().add(nombre);
+                    }
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
+                }
             }
         }).start();
     }
 
     @FXML
-    public void showNuevaReserva(ActionEvent event) {
+    public void showNuevaReserva(ActionEvent event) throws ErrorGuardarCambios {
+        String nombreCliente = txtNombre.getText();
+        LocalDate fechaSolicitud = dateSolicitud.getValue();
+        String documento = txtDocumento.getText();
+        LocalDate fechaViaje = dateViaje.getValue();
+        String nombreGuia = boxGuia.getValue();
+        Short numPer = boxNumPersonas.getValue();
+        String paquete = boxPaquete.getValue();
+        EstadoReserva estado = boxEstado.getValue();
 
+        Cliente user = AgenciaUQ.getInstance().clienteEnLista(nombreCliente);
+        if (user == null) {
+            ArchivoUtils.mostrarMensaje("Error", "Cliente no encontrado", "El cliente ingresado no está en la lista.", Alert.AlertType.ERROR);
+            throw new ErrorGuardarCambios("el cliente no esta en la lista");
+        }
+        GuiaTuristico guia = AgenciaUQ.getInstance().obtenerGuiaPorNombre(nombreGuia);
+        if (guia == null) {
+            ArchivoUtils.mostrarMensaje("Error", "Guia no encontrado", "El guia ingresado no está en la lista.", Alert.AlertType.ERROR);
+            throw new ErrorGuardarCambios("el cliente no esta en la lista");
+        }
+        PaqueteTuristico nuevoPaquete = AgenciaUQ.getInstance().obtenerPaquetePorNombre(paquete);
+        if (nuevoPaquete == null) {
+            ArchivoUtils.mostrarMensaje("Error", "Paquete no encontrado", "El Paquete ingresado no está en la lista.", Alert.AlertType.ERROR);
+            throw new ErrorGuardarCambios("el cliente no esta en la lista");
+        }
+
+        // Crear la reserva
+        try {
+            AgenciaUQ.getInstance().crearReserva(fechaSolicitud, fechaViaje, documento, numPer, nuevoPaquete, user, guia, estado);
+            ArchivoUtils.mostrarMensaje("Éxito", "Reserva exitosa", "La reserva se ha creado correctamente.", Alert.AlertType.INFORMATION);
+        } catch (AtributoVacioException | FechaNoValidaException | CupoInvalidoException | IOException e) {
+            ArchivoUtils.mostrarMensaje("Error", "Error al crear reserva", "Hubo un error al intentar crear la reserva.", Alert.AlertType.ERROR);
+            e.printStackTrace();
+        }
     }
-
 }
